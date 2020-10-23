049f908a1d4c73c8a8460f4e1f69b3fc0a9e7bc9
Akshay Mhatre <akshaym005@gmail.com>
2020-10-23 09:37:33 +0530

    Updates

---
+++ precache-manifest.06d33c06ebe26ff03e2364a54a27f729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5814f7ab43355b677a8a8d3f1236fe2",
+    "url": "/covid-tracker-maharashtra/index.html"
+  },
+  {
+    "revision": "c1db6531c1c92886f36e",
+    "url": "/covid-tracker-maharashtra/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19e23c685e229b0e9a93",
+    "url": "/covid-tracker-maharashtra/static/js/2.f28a2d67.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/covid-tracker-maharashtra/static/js/2.f28a2d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1db6531c1c92886f36e",
+    "url": "/covid-tracker-maharashtra/static/js/main.826d386e.chunk.js"
+  },
+  {
+    "revision": "96d7e74fa81764e14325",
+    "url": "/covid-tracker-maharashtra/static/js/runtime-main.da974fa5.js"
+  }
+]);
